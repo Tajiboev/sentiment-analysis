c0e40e706e420f599add511969b9df1e83406785
Tajiboev <mukhammadjon.tajiboev@gmail.com>
2021-07-26 11:48:15 +0500

    server log

--- source/server.ts
+++ source/server.ts
@@ -3,6 +3,7 @@ import app from "./app";
 
 const start = async () => {
 	try {
+		console.log("Starting server");
 		const port = process.env.PORT || 5000;
 		const server = createServer(app);
 		server.listen(port, () => {
