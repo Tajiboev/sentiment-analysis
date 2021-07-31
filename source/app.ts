a4bee88f1b5fbba15e8631e8d99a93568fd5eace
Tajiboev <mukhammadjon.tajiboev@gmail.com>
2021-07-31 23:01:05 +0500

    added rate limiter

--- source/app.ts
+++ source/app.ts
@@ -4,15 +4,32 @@ import compression from "compression";
 import helmet from "helmet";
 import HTTPError from "./utils/httpError";
 import router from "./router";
+import rateLimit from "express-rate-limit";
+import slowDown from "express-slow-down";
 
 const app = express();
 
+app.set("trust proxy", 1);
 app.disable("etag");
 app.use(helmet());
 app.use(cors());
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const limiter = rateLimit({
+	windowMs: 1 * 60 * 1000, // 1 minute
+	max: 2, // limit each IP to 100 requests per windowMs
+});
+
+const speedLimiter = slowDown({
+	windowMs: 10 * 60 * 1000, // 10 minutes
+	delayAfter: 2, // allow 100 requests per 10 minutes, then...
+	delayMs: 500, // begin adding 500ms of delay per request above 100:
+});
+
+app.use(limiter);
+app.use(speedLimiter);
 
 //route
 app.use(router);
