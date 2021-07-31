817dcb2afd0eee2e18cac90b02279a0d58224281
Tajiboev <mukhammadjon.tajiboev@gmail.com>
2021-07-31 23:49:06 +0500

    new build

--- build/app.js
+++ build/app.js
@@ -18,13 +18,27 @@ const compression_1 = __importDefault(require("compression"));
 const helmet_1 = __importDefault(require("helmet"));
 const httpError_1 = __importDefault(require("./utils/httpError"));
 const router_1 = __importDefault(require("./router"));
+const express_rate_limit_1 = __importDefault(require("express-rate-limit"));
+const express_slow_down_1 = __importDefault(require("express-slow-down"));
 const app = express_1.default();
+app.set("trust proxy", 1);
 app.disable("etag");
 app.use(helmet_1.default());
 app.use(cors_1.default());
 app.use(compression_1.default());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(express_1.default.json());
+const limiter = express_rate_limit_1.default({
+    windowMs: 1 * 60 * 1000,
+    max: 20,
+});
+const speedLimiter = express_slow_down_1.default({
+    windowMs: 1 * 60 * 1000,
+    delayAfter: 15,
+    delayMs: 500,
+});
+app.use(limiter);
+app.use(speedLimiter);
 app.use(router_1.default);
 app.use((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     next(new httpError_1.default("Not found", 404));
