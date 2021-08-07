305cc2bcb0c250641feb22eda337bec8704c91c9
Tajiboev <mukhammadjon.tajiboev@gmail.com>
2021-08-07 16:19:30 +0500

    strict boolean check

--- source/utils/validationSchemas.ts
+++ source/utils/validationSchemas.ts
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 const analyzeSchema = Joi.object({
 	text: Joi.string().required(),
-	splitter: Joi.string(),
-});
+	split: Joi.boolean().sensitive(false).required(),
+}).strict();
 
 export { analyzeSchema };
